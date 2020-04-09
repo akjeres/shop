f9e9348ac2b106028e6a6291e30313f9bf25c218
akjeres <akjeres@gmail.com>
2020-04-09 15:18:17 +0300

    1st Lesson

---
+++ src/Pages/ProductList/Blocks/Card.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Card as CardComponent } from 'antd';
+import ReactCardFlip from "react-card-flip";
+import { Link } from "react-router-dom";
+
+const { Meta } = CardComponent;
+
+export const Card = (props) => {
+    const { el } = props;
+    const [isFlipped, changeFlipped] = useState(false);
+    const onChangeFlipped = () => {
+        console.log('isFlipped: ', isFlipped);
+        changeFlipped( !isFlipped );
+    };
+
+    return (
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+            <CardComponent
+                hoverable
+                style={ styles.cardStyle }
+                cover={
+                    <img alt={ el.name } src={ el.image }
+                         style={ styles.imageStyle }/>
+                }
+                onClick={ onChangeFlipped }
+            >
+                <Link to="/details">
+                    <Meta title={ el.name } />
+                </Link>
+            </CardComponent>
+            <CardComponent
+                hoverable
+                style={ styles.cardStyle }
+                cover={
+                    <img alt={ el.name } src={ el.image }
+                         style={ styles.imageStyle }/>
+                }
+                onClick={ onChangeFlipped }
+            >
+                <Link to="/details">
+                    <Meta title="Back Side" />
+                </Link>
+            </CardComponent>
+        </ReactCardFlip>
+    );
+};
+
+const styles = {
+    cardStyle: {
+        width: 240,
+        cursor: 'pointer',
+    },
+    imageStyle: {
+        height: 240,
+        objectFit: 'contain'
+    }
+};
