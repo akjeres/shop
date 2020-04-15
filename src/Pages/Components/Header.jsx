8127c1077a5411283453d76dc70e216457b8dbf2
akjeres <akjeres@gmail.com>
2020-04-15 10:59:50 +0300

    Header navigation button events updated

--- src/Pages/Components/Header.jsx
+++ src/Pages/Components/Header.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { PageHeader, Button } from 'antd';
 import { ShoppingCartOutlined, HomeOutlined } from '@ant-design/icons';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const HeaderComponent = () => {
+    const history = useHistory();
     return (
         <PageHeader
             ghost={ false }
-            onBack={() => window.history.back() }
+            onBack={() => {
+                history.push("/");
+            }}
             title="Shop Awesome"
             backIcon={ <HomeOutlined key="home-icon" /> }
             className="site-page-header"
             extra={
                 [
-                    <Link to="/cart" key="cart-btn">
-                        <Button
-                            type="primary"
-                            shape="circle"
-                        >
-                            <ShoppingCartOutlined key="cart-icon" />
-                        </Button>
-                    </Link>,
+                    <Button
+                        type="primary"
+                        shape="circle"
+                        onClick={() => {
+                            history.push("/cart");
+                        }}
+                    >
+                        <ShoppingCartOutlined key="cart-icon" />
+                    </Button>,
                 ]
             }
         >
